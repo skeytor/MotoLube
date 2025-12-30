3cfcf604c3023f73e902e3d92389e066e290d64f
Rober Guerrero <roberguerreromendoza@gmail.com>
2025-12-30 08:38:34 -0400

    feat: add AppDbContext implementation

---
+++ src/Core/MotoLube.Persistence/Abstractions/IAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotoLube.Domain.Entities;
+
+namespace MotoLube.Persistence.Abstractions;
+
+public interface IAppDbContext
+{
+    DbSet<Product> Products { get; }
+    DbSet<Category> Categories { get; }
+    DbSet<Brand> Brands { get; }
+    DbSet<Customer> Customers { get; }
+    DbSet<Supplier> Suppliers { get; }
+    DbSet<Inbound> Inbounds { get; }
+    DbSet<InboundItem> InboundItems { get; }
+    DbSet<Outbound> Outbounds { get; }
+    DbSet<OutboundItem> OutboundItems { get; }
+}
